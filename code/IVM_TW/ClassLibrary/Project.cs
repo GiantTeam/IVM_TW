71365cd0c42c9f1f9c2453fcb28f97338175480e
CharisHsiao <905391834@qq.com>
2016-04-27 22:15:24 +0800

    添加类库和对应的类测试项目
    Signed-off-by: CharisHsiao <905391834@qq.com>

---
+++ code/IVM_TW/ClassLibrary/Project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary
+{
+     public class Project : IComparable
+    {
+        public int intId;  //理财产品编号
+        public int intTime; //起投期限
+        public double dblRate; //项目收益率
+        public double dblMoney;  //项目起投金额
+        public string strLink; //理财产品链接
+
+        public Project(int intId)
+        {
+            this.intId = intId;
+            this.intTime = 0;
+            this.dblMoney = 0;
+            this.dblMoney =0;
+            this.strLink = ""; 
+        }
+        public int CompareTo(Object rhs)
+        {
+            Project r = (Project)rhs;
+            return this.intId.CompareTo(r.intId);
+        }
+
+    }
+}
---
+++ code/IVM_TW/ClassLibrary/ProjectList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary
+{
+    using System.Collections;
+    class ProjectList
+    {
+
+        //Use Facade Pattern
+        ArrayList proArray = new ArrayList();
+        public void Add(Project mProject)
+        {
+            proArray.Add(mProject);
+        }
+        public int Count()
+        {
+            return proArray.Count;
+        }
+        public void Sort()
+        {
+             proArray.Sort();
+        }
+    
+    }
+
+}

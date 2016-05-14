d86adfaae93a102d25d91f10fd3f496bae06d198
linduan <1510596238@qq.com>
2016-05-14 11:39:02 +0800

    Signed-off-by: linduan <1510596238@qq.com>修改画图框架

--- code/IVM_TM/VM/frmChart.Designer.cs
+++ code/IVM_TM/VM/frmChart.Designer.cs
@@ -37,15 +37,11 @@
             this.txtC3 = new System.Windows.Forms.TextBox();
             this.txtC4 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.btnCReturn = new System.Windows.Forms.Button();
+            this.btnC1 = new System.Windows.Forms.Button();
+            this.btnC2 = new System.Windows.Forms.Button();
+            this.btnC3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // lblCMoney
@@ -118,58 +114,15 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(16, 104);
+            this.pictureBox1.Location = new System.Drawing.Point(48, 96);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(256, 168);
+            this.pictureBox1.Size = new System.Drawing.Size(800, 500);
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(296, 104);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(272, 168);
-            this.pictureBox2.TabIndex = 9;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Location = new System.Drawing.Point(600, 104);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(272, 168);
-            this.pictureBox3.TabIndex = 10;
-            this.pictureBox3.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(112, 280);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "收益率";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(408, 288);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "已投金额";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(704, 280);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 12);
-            this.label3.TabIndex = 13;
-            this.label3.Text = "已获收益";
-            // 
             // btnCReturn
             // 
-            this.btnCReturn.Location = new System.Drawing.Point(696, 32);
+            this.btnCReturn.Location = new System.Drawing.Point(712, 56);
             this.btnCReturn.Name = "btnCReturn";
             this.btnCReturn.Size = new System.Drawing.Size(75, 23);
             this.btnCReturn.TabIndex = 14;
@@ -177,17 +130,45 @@
             this.btnCReturn.UseVisualStyleBackColor = true;
             this.btnCReturn.Click += new System.EventHandler(this.btnCReturn_Click);
             // 
+            // btnC1
+            // 
+            this.btnC1.Location = new System.Drawing.Point(576, 16);
+            this.btnC1.Name = "btnC1";
+            this.btnC1.Size = new System.Drawing.Size(75, 23);
+            this.btnC1.TabIndex = 15;
+            this.btnC1.Text = "收益";
+            this.btnC1.UseVisualStyleBackColor = true;
+            this.btnC1.Click += new System.EventHandler(this.btnC1_Click);
+            // 
+            // btnC2
+            // 
+            this.btnC2.Location = new System.Drawing.Point(576, 56);
+            this.btnC2.Name = "btnC2";
+            this.btnC2.Size = new System.Drawing.Size(75, 23);
+            this.btnC2.TabIndex = 16;
+            this.btnC2.Text = "投资";
+            this.btnC2.UseVisualStyleBackColor = true;
+            this.btnC2.Click += new System.EventHandler(this.btnC2_Click);
+            // 
+            // btnC3
+            // 
+            this.btnC3.Location = new System.Drawing.Point(712, 16);
+            this.btnC3.Name = "btnC3";
+            this.btnC3.Size = new System.Drawing.Size(75, 23);
+            this.btnC3.TabIndex = 17;
+            this.btnC3.Text = "收益率";
+            this.btnC3.UseVisualStyleBackColor = true;
+            this.btnC3.Click += new System.EventHandler(this.btnC3_Click);
+            // 
             // frmChart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(901, 310);
+            this.ClientSize = new System.Drawing.Size(901, 630);
+            this.Controls.Add(this.btnC3);
+            this.Controls.Add(this.btnC2);
+            this.Controls.Add(this.btnC1);
             this.Controls.Add(this.btnCReturn);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtC4);
             this.Controls.Add(this.txtC3);
@@ -200,8 +181,6 @@
             this.Name = "frmChart";
             this.Text = "图表分析";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -218,11 +197,9 @@
         private System.Windows.Forms.TextBox txtC3;
         private System.Windows.Forms.TextBox txtC4;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnCReturn;
+        private System.Windows.Forms.Button btnC1;
+        private System.Windows.Forms.Button btnC2;
+        private System.Windows.Forms.Button btnC3;
     }
 }
--- code/IVM_TM/VM/frmChart.cs
+++ code/IVM_TM/VM/frmChart.cs
@@ -12,6 +12,115 @@ namespace VM
 {
     public partial class frmChart : Form
     {
+        int[] date = new int[13] { 0, 20, 13, 30, 15, 7, 16, 27, 8, 9, 30, 21, 12 };
+        int[] D = new int[13] { 0, 20, 13, 30, 15, 7, 16, 27, 8, 9, 20, 21, 12 };
+        int length = 12;
+        int smonth = 7;
+        int syear = 2013;
+        int yAdd = 10;
+        public int diff(int x, int y)
+        {
+            if ((x - y) % 2 == 0)
+            {
+                y = y + 30 * (x % 3) + 60 * (x % 6);
+            }
+            if (x < y)
+                return y - x;
+            else
+                return (30 - x) + y;
+        }
+        public int sum(int i)
+        {
+            int j;
+            int all = 0;
+            if (i == 1)
+            {
+                return date[1];
+            }
+            for (j = 1; j < i + 1; j++)
+                all += diff(date[j - 1], date[j]);
+            return all;
+        }
+        public int ysum(int i)
+        {
+            return (D[i] * 3 + 10) * 4;
+        }
+
+        public void paint(string strtitle, string strx, string stry, int[] d, int yadd)
+        {
+            //画图初始化
+            Bitmap bmap = new Bitmap(800, 500);
+            Graphics gph = Graphics.FromImage(bmap);
+            gph.Clear(Color.White);
+            smonth = smonth - 1;
+
+            //三点确定坐标框架
+            PointF cpt = new PointF(40, 440);//中心点O
+            PointF ex = new PointF(760, cpt.Y);//x轴端点
+            PointF ey = new PointF(cpt.X, 40);//y轴端点
+            PointF[] xpt = new PointF[3] { new PointF(ex.X + 15, ex.Y), new PointF(ex.X, ex.Y - 8), new PointF(ex.X, ex.Y + 8) };//x轴三角形
+            PointF[] ypt = new PointF[3] { new PointF(ey.X, ey.Y - 15), new PointF(ey.X - 8, ey.Y), new PointF(ey.X + 8, ey.Y) };//y轴三角形
+            gph.DrawString(strtitle, new Font("宋体", 14), Brushes.Black, new PointF(ex.X - 80, ey.Y - 30));//图表标题
+            //画x轴
+            gph.DrawLine(Pens.Black, cpt.X, cpt.Y, ex.X, ex.Y);
+            gph.DrawPolygon(Pens.Black, xpt);
+            gph.FillPolygon(new SolidBrush(Color.Black), xpt);
+            gph.DrawString(strx, new Font("宋体", 12), Brushes.Black, new PointF(ex.X + 5, ex.Y + 5));
+            //画y轴
+            gph.DrawLine(Pens.Black, cpt.X, cpt.Y, ey.X, ey.Y);
+            gph.DrawPolygon(Pens.Black, ypt);
+            gph.FillPolygon(new SolidBrush(Color.Black), ypt);
+            gph.DrawString(stry, new Font("宋体", 12), Brushes.Black, new PointF(0, 7));
+
+            //绘制XY轴内容
+            for (int i = 1; i < length * 2 + 1; i++, smonth++)
+            {
+                if (i == 1)
+                {
+                    gph.DrawString((syear).ToString() + "年", new Font("宋体", 11), Brushes.Black, new PointF(cpt.X + 30 * i - 45, cpt.Y + 20));
+                    syear++;
+                }
+                //画y轴刻度：平均下来40点/单位长度
+                if (i < 11)
+                {
+                    gph.DrawString((yadd * i).ToString(), new Font("宋体", 11), Brushes.Black, new PointF(cpt.X - 40, cpt.Y - i * 40 - 6));
+                    gph.DrawLine(Pens.Black, cpt.X - 3, cpt.Y - i * 40, cpt.X, cpt.Y - i * 40);
+                }
+                //画x轴刻度:平均下来一天一个像素点
+                gph.DrawString(((smonth % 12) + 1).ToString() + "月", new Font("宋体", 11), Brushes.Black, new PointF(cpt.X + 30 * i - 30, cpt.Y + 3));
+                gph.DrawLine(Pens.Black, cpt.X + 30 * i, cpt.Y - 3, cpt.X + 30 * i, cpt.Y);
+                if ((smonth % 12) + 1 == 1)
+                {
+                    gph.DrawString((syear).ToString() + "年", new Font("宋体", 11), Brushes.Black, new PointF(cpt.X + 30 * i - 45, cpt.Y + 20));
+                    syear++;
+                }
+
+            }
+
+            for (int i = 1; i <= length; i++)
+            {
+                /*画点
+                 * p[i]为数组中按日期排第i个数据，p[0]为第一天的数据，diff(p[i],p[0])为第i天与第0天天数差
+                 * p[0].X=cpt.X +p[0].Days;
+                 * p[i].X=p[0].X+diff(p[i],p[0]);
+                 * p[i].Y=cpt.Y+p[i].Y*0.4;p[i].Y已转化为0-100之间的数
+                 */
+                gph.DrawEllipse(Pens.Black, cpt.X + sum(i), cpt.Y - ysum(i), 3, 3);
+                gph.FillEllipse(new SolidBrush(Color.Black), cpt.X + sum(i), cpt.Y - ysum(i), 3, 3);
+                //画数值
+                gph.DrawString((d[i] * 3 + 10).ToString(), new Font("宋体", 11), Brushes.Black, new PointF(cpt.X + sum(i), cpt.Y - ysum(i)));
+                //画折线
+                if (i > 1)
+                {
+                    gph.DrawLine(Pens.Red, cpt.X + sum(i - 1), cpt.Y - ysum(i - 1), cpt.X + sum(i) - 1, cpt.Y - ysum(i - 1));
+                    gph.DrawLine(Pens.Red, cpt.X + sum(i) - 1, cpt.Y - ysum(i - 1), cpt.X + sum(i), cpt.Y - ysum(i));
+                }
+            }
+            //保存输出图片
+            //bmap.Save(Response.OutputStream, ImageFormat.Gif);
+            pictureBox1.Image = bmap;
+        }
+//******************************************************************************************
         public frmChart()
         {
             InitializeComponent();
@@ -21,5 +130,22 @@ namespace VM
         {
             this.Close();
         }
+
+        private void btnC1_Click(object sender, EventArgs e)
+        {
+            paint("收益", "日期", "单位：万元", D,2);
+        }
+
+        private void btnC2_Click(object sender, EventArgs e)
+        {
+            paint("投资", "日期", "单位：万元", D, 2);
+        }
+
+        private void btnC3_Click(object sender, EventArgs e)
+        {
+            paint("收益率", "日期", "单位：%", D,2);
+        }
+
+   
     }
 }

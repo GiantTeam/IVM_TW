3302994e3334550f30917457e441f8d129a7827e
GitHub-ldf <m13750057043@163.com>
2016-05-25 11:41:57 +0800

    Signed-off-by: GitHub-ldf <m13750057043@163.com>

--- code/IVM_TM/ControlClass/GetWebContent.cs
+++ code/IVM_TM/ControlClass/GetWebContent.cs
@@ -4,20 +4,30 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ControlClass
 {
     public class GetWebContent
     {
+        public static Boolean mark = false;
         //获取网页内容
         public static string LoadDataFromWeb(string url)
         {
-            //WebClient ourSearchWeb = new WebClient();
-            //ourSearchWeb.Credentials = CredentialCache.DefaultCredentials;
-            //Byte[] webPageData = ourSearchWeb.DownloadData(url);
-            //string webPageHtml = Encoding.UTF8.GetString(webPageData);
-            //Console.Write(webPageHtml);
-           string webPageHtml = "";
+            string webPageHtml = "";
+            try {
+                WebClient ourSearchWeb = new WebClient();
+                ourSearchWeb.Credentials = CredentialCache.DefaultCredentials;
+                Byte[] webPageData = ourSearchWeb.DownloadData(url);
+                webPageHtml = Encoding.UTF8.GetString(webPageData);
+                mark = true;
+            } catch (Exception e)
+            {
+                MessageBox.Show("网络异常");
+                mark = false;
+            }
+           // Console.Write(webPageHtml);
+          
             return webPageHtml;            
         }
     }
